e8825c99d7cbfe271ebb25006afb5471095f6006
unknown <manuelverolo@gmail.com>
2021-08-03 23:36:40 -0300

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,67 @@
+var posicionMarcada = "ninguna";
+
+var marcoTablero = document.getElementById('border-tablero');
+
+//Event para ver click casilla
+marcoTablero.addEventListener('click', e => { seleccionCelda(e)})
+var seleccionCelda = e => {
+    if (e.target.classList.contains('ficha-blanca')){         
+        if (turnoJugador == 1) {
+            if(e.target.classList.contains('select-box') !== true){
+                e.target.classList.add("select-box");
+                if (posicionMarcada !== "ninguna") {
+                    var casillaDesmarca = document.getElementById(posicionMarcada);
+                    casillaDesmarca.classList.remove("select-box");
+                }
+            }
+            posicionMarcada = e.target.id;                       
+        } else if (turnoJugador == 2) {                          
+            window.alert("No es tu turno");
+        }
+
+    } else if (e.target.classList.contains('ficha-roja')){     //----->>>>> seleccion de ficha negra
+        if (turnoJugador == 2) {
+            if(e.target.classList.contains('select-box') !== true){
+                e.target.classList.add("select-box");
+                if (posicionMarcada !== "ninguna") {
+                    var casillaDesmarca = document.getElementById(posicionMarcada);
+                    casillaDesmarca.classList.remove("select-box");
+                }
+            }
+            posicionMarcada = e.target.id;                       
+        } else if (turnoJugador == 1) {                          
+            window.alert("No es tu turno");
+        }
+    } else {                                            
+        if (posicionMarcada !== "ninguna") {            
+            var casillaDesmarca = document.getElementById(posicionMarcada);
+            if ( casillaDesmarca.classList.contains('ficha-blanca')  && turnoJugador == 1  && casillaValida('blancas', posicionMarcada, e.target.id)) { 
+
+                e.target.classList.add("ficha-blanca");     
+    
+                casillaDesmarca.classList.remove("ficha-blanca");  
+                casillaDesmarca.classList.remove("select-box"); 
+                
+                turnoJugador = 2;        
+                document.getElementById('turno-jugador').textContent = 'Es el turno del jugador: ' + turnoJugador;
+                console.log('')
+                posicionMarcada = e.target.id;                       
+            }
+            else if(casillaDesmarca.classList.contains('ficha-roja') 
+                    && turnoJugador == 2 
+                    && casillaValida('negras', posicionMarcada, e.target.id) ) {
+                
+                e.target.classList.add("ficha-roja"); 
+
+                casillaDesmarca.classList.remove("ficha-roja");  
+                casillaDesmarca.classList.remove("select-box"); 
+                
+    
+                turnoJugador = 1; 
+                document.getElementById('turno-jugador').textContent = 'Es el turno del jugador: ' + turnoJugador;
+                
+                posicionMarcada = e.target.id;                       
+            }
+        }
+    }
+}
---
+++ js/minidamas.js
@@ -0,0 +1,198 @@
+function nuevaPartida() {
+    if (window.confirm("Se perderán los datos actuales de la partida, desea continuar?")) {
+        nuevoTablero();
+        window.alert("A comenzado una nueva partida");
+    }
+}
+
+//CARGAS INICIALES PARA PARTIDA NUEVA
+function nuevoTablero() {
+    ArrayInicial = [
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0],
+        [0,1,0,1,0,1,0,1],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [2,0,2,0,2,0,2,0],
+        [0,2,0,2,0,2,0,2],
+        [2,0,2,0,2,0,2,0],
+    ];
+
+    puntosJugador1 = 12;
+    puntosJugador2 = 12;
+
+    //Turno iniciar
+    turnoJugador = 1;
+
+    insert_fichas(ArrayInicial, puntosJugador1, puntosJugador2, turnoJugador);
+}
+//FIN CARGA INCIAL PARA PARTIDA NUEVA
+
+
+function tablero() {
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    for (var i = 0; i < 8; i++) {
+        //creo la fila
+        var fila = document.createElement("tr");
+        for (var j = 0; j < 8; j++) {
+            //creo la casilla
+            var celda = document.createElement("td");
+            celda.id = i + "-" + j;
+            indice = i + j;
+
+            //Calculo para definir el color de las casillas
+            //Par = negro, impar = blanca
+            if (indice%2 == 0) {               
+                celda.className = 'casilla-blanca';         
+            } else {
+                celda.className = 'casilla-negra';   
+            }
+            fila.appendChild(celda);
+        }
+        tblBody.appendChild(fila);
+    }
+    tabla.appendChild(tblBody);
+    marcoTablero.appendChild(tabla);
+}
+
+//Inserto fichas en el tablero
+function insert_fichas(ArrayTablero, puntosJugador1, puntosJugador2, turnoJugador) {
+
+    document.getElementById('puntos1').value = puntosJugador1;
+    document.getElementById('puntos2').value = puntosJugador2;
+    //Muestra jugador incial --
+    document.getElementById('turno-jugador').textContent = 'Es el turno del jugador: ' + turnoJugador;
+
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            var nombreCelda = i + "-" + j;
+            var celda = document.getElementById(nombreCelda);
+            celda.id = i + "-" + j;
+            indice = i + j;
+            
+            if ( ArrayTablero[i][j] == 1 ) {
+                celda.classList.add("ficha-blanca");                
+            }
+            else if( ArrayTablero[i][j] == 2 ) {
+                celda.classList.add("ficha-roja");
+            }
+        }   
+    }    
+}
+
+
+//Function para validar donde ubicar ficha - (parametros desde app.js)
+function casillaValida(colorFichas, posicionAnterior, posicionNueva){
+    
+    //Varibales de comparacion 
+    var filaAnterior = parseInt(posicionAnterior.substring(0,1));
+    var columnaAnterior = parseInt(posicionAnterior.substring(2));
+    var filaNueva = parseInt(posicionNueva.substring(0,1));
+    var columnaNueva = parseInt(posicionNueva.substring(2));
+
+    //verifico segun el color de ficha que toca mover
+    if (colorFichas == 'blancas'){       
+        if(filaAnterior < 8  && filaNueva == (filaAnterior+1)  && Math.abs(columnaAnterior-columnaNueva) == 1){
+            return true;
+        }
+        else if(filaAnterior < 7  && filaNueva == (filaAnterior+2) && Math.abs(columnaAnterior-columnaNueva) == 2){
+            var posicionPosibleFicha = (filaAnterior+1) + "-" + (columnaAnterior + ((columnaNueva-columnaAnterior)/2));
+            var casillaVerificar = document.getElementById(posicionPosibleFicha);
+            if (casillaVerificar.classList.contains('ficha-roja')){ 
+                casillaVerificar.classList.remove("ficha-roja");
+                document.getElementById('puntos2').value -= 1;
+                if(document.getElementById('puntos2').value == 0){
+                    window.alert("GANADORAS: BLANCAS");
+                }else{
+                    return true;
+                }
+            }
+        }
+    }   
+    else if (colorFichas == 'negras') {
+        console.log('mueven las negras');
+        if(filaAnterior > 1 
+            && filaNueva == (filaAnterior-1) 
+            && Math.abs(columnaAnterior-columnaNueva) == 1){
+            if (columnaAnterior > 1){
+                console.log('se mueve desde la columna mayor a 1, hay celda libre avance columna anterior')
+            }
+            else if (columnaAnterior < 8){
+                console.log('se mueve desde la columna menor a 8, hay celda libre avance columna siguiente')
+            }
+            return true;
+        }
+        else if(filaAnterior > 2 
+            && filaNueva == (filaAnterior-2) 
+            && Math.abs(columnaAnterior-columnaNueva) == 2){
+            var posicionPosibleFicha = (filaAnterior-1) + "-" + (columnaAnterior + ((columnaNueva-columnaAnterior)/2));
+            console.log(posicionPosibleFicha);
+            var casillaVerificar = document.getElementById(posicionPosibleFicha);
+            if (casillaVerificar.classList.contains('ficha-blanca')){ 
+                casillaVerificar.classList.remove("ficha-blanca");
+                document.getElementById('puntos1').value -= 1;
+                if(document.getElementById('puntos1').value == 0){
+                    window.alert("GANADORAS: ROJAS");
+                }else{
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function guardar_partida() {
+
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            var nombreCelda = i + "-" + j;
+            var celda = document.getElementById(nombreCelda);
+            if ( celda.classList.contains('ficha-blanca') ) {               
+                ArrayInicial[i][j] = 1;     
+            } 
+            else if ( celda.classList.contains('ficha-roja') ) {
+                ArrayInicial[i][j] = 2;     
+            }
+            else {
+                ArrayInicial[i][j] = 0;       
+            }
+        }   
+    }  
+    var ArrayInicialString = JSON.stringify(ArrayInicial);
+    //GUARDO LOS DATOS AL LOCAL STORAGE
+    localStorage.setItem("Partida1.Puntos1", document.getElementById('puntos1').value);
+    localStorage.setItem("Partida1.Puntos2", document.getElementById('puntos2').value);
+    localStorage.setItem("Partida1.Jugador1",document.getElementById('name-1').value);
+    localStorage.setItem("Partida1.Jugador2",document.getElementById('name-2').value);
+    localStorage.setItem("Partida1.Juego", ArrayInicialString);
+    localStorage.setItem("Partida1.TurnoJugador", turnoJugador);
+    window.alert("Partida guardada");
+
+}
+
+function recuperar_partida_guardada() {
+    if (window.confirm("Desea cargar la partida?")) {
+        var puntosJugador1 = localStorage.getItem("Partida1.Puntos1");
+        var puntosJugador2 = localStorage.getItem("Partida1.Puntos2");
+        var ArrayInicial =  JSON.parse(localStorage.getItem("Partida1.Juego"));
+        var turnoJugador = localStorage.getItem("Partida1.TurnoJugador");
+        insert_fichas(ArrayInicial, puntosJugador1, puntosJugador2, turnoJugador);
+    }
+}
+
+function guardarJugadores(){
+    var nombre1 = document.getElementById('name-1').value;
+    var nombre2 = document.getElementById('name-2').value;
+
+    document.getElementById('jugador-nombre1').textContent = nombre1;
+    document.getElementById('jugador-nombre2').textContent = nombre2;
+
+}
+
+
+
+
+
+
